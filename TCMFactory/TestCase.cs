f91d118f204ab1cbfecc1949acfb5c1b860b6eeb
debasish <debasish.pradhan@cigniti.com>
2019-12-20 17:45:05 +0530

    TCMFactory project and related files to be checked in

---
+++ TCMFactory/TestCase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMTFactory;
+namespace TCMFactory
+{
+    public class TestCase
+    {
+        private String testCategory = null;
+        private String testCaseId = null;
+        private String testCaseName = null;
+        private String testCaseDescription = null;       
+        private ResultStatus iTestExecutionStatus;
+        private ulong iTestExecutionTimeinMS = 0;
+
+
+        public String TestCategory
+        {
+            get { return this.testCategory; }
+            set { this.testCategory = value; }
+        }
+
+        public String TestCaseId
+        {
+            get { return this.testCaseId; }
+            set { this.testCaseId = value; }
+        }
+
+        public String TestCaseName
+        {
+            get { return this.testCaseName; }
+            set { this.testCaseName = value; }
+        }
+
+        public String TestCaseDescription
+        {
+            get { return this.testCaseDescription; }
+            set { this.testCaseDescription = value; }
+        }
+
+        public ResultStatus TestExecutionStatus
+        {
+            get { return this.iTestExecutionStatus; }
+            set { this.iTestExecutionStatus = value; }
+        }
+             
+        public String convertTestExecutionStatusToString()
+        {
+            String testStatus = null;
+            switch(this.TestExecutionStatus)
+            {
+                case ResultStatus.Passed :
+                    testStatus = "Passed";
+                    break;
+
+                case ResultStatus.Failed :
+                    testStatus = "Failed";
+                    break;
+            }
+
+            return testStatus;
+        }
+    }
+}
---
+++ TCMFactory/TestExecutionManagement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMTFactory;
+namespace TCMFactory
+{
+    public class TestExecutionManagement
+    {
+
+        public static int GetTotalPassTestCases(IList<TestCase> listTestCases)
+        {
+            int totalPass = 0;
+            foreach (TestCase tc in listTestCases)
+            {
+                if (tc.TestExecutionStatus == ResultStatus.Passed)
+                {
+                    totalPass++;
+                }
+            }
+
+            return totalPass;
+        }
+        
+
+        public static int GetTotalFailedTestCases(IList<TestCase> listTestCases)
+        {
+            int totalFail = 0;
+            foreach (TestCase tc in listTestCases)
+            {
+                if (tc.TestExecutionStatus == ResultStatus.Failed)
+                {
+                    totalFail++;
+                }
+            }
+
+            return totalFail;
+        }
+
+        public static IList<TestCase> GetFailedTestCases(IList<TestCase> listTestCases)
+        {
+            IList<TestCase> listFailedTestCases = new List<TestCase>();
+            foreach (TestCase tc in listTestCases)
+            {
+                if (tc.TestExecutionStatus == ResultStatus.Passed)
+                {
+                    listFailedTestCases.Add(tc);
+                }
+            }
+
+            return listFailedTestCases;
+        }
+
+        public static IList<TestCase> GetPassedTestCases(IList<TestCase> listTestCases)
+        {
+            IList<TestCase> listPassedTestCases = new List<TestCase>();
+            foreach (TestCase tc in listTestCases)
+            {
+                if (tc.TestExecutionStatus == ResultStatus.Passed)
+                {
+                    listPassedTestCases.Add(tc);
+                }
+            }
+            return listPassedTestCases;
+        }
+                
+    }
+}
